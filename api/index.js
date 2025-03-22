b21867964f409f4105dcab29a3edd512ca68f6b8
j-preim <j.preimesberger@gmail.com>
2025-03-22 16:55:49 -0500

    get

--- api/index.js
+++ api/index.js
@@ -10,12 +10,31 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://api.sportradar.us/ncaamb/trial/v8/en/tournaments/56befd3f-4024-47c4-900f-892883cc1b6b/schedule.json?api_key=luKiaKnnuM5AyqYEimwuW2YnoRK9ks4F6wYPP307"],
+    origin: [
+      "http://localhost:3000",
+      "https://api.sportradar.us/ncaamb/trial/v8/en/tournaments/56befd3f-4024-47c4-900f-892883cc1b6b/schedule.json?api_key=luKiaKnnuM5AyqYEimwuW2YnoRK9ks4F6wYPP307",
+    ],
   })
 );
 
 app.get("/schedule", function (req, res) {
   res.sendFile(path.join(__dirname, "..", "static", "initSched.json"));
+});
+
+app.get("/getSchedule", async function getData() {
+  const url =
+    "https://api.sportradar.us/ncaamb/trial/v8/en/tournaments/56befd3f-4024-47c4-900f-892883cc1b6b/schedule.json?api_key=luKiaKnnuM5AyqYEimwuW2YnoRK9ks4F6wYPP307";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
 });
 
 app.post("/test-cors-post", function (req, res) {
